3fcb0cffc99c3b0c7f3a49a8860d45e496070113
Braden Carter <bradencarter@outlook.com>
2025-12-21 19:53:17 -0500

    removing non-src files

--- app/projects/actions/IProjectRetriever.ts
+++
@@ -1,6 +0,0 @@
-import { Project } from "../data/projects";
-
-export interface IProjectRetriever {
-    getAllProjects: () => Record<string, Project>;
-    getProjectById: (id: string) => Project | null;
-}
